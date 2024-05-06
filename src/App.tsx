d9e9ddbdbfae6c10f8d3f32bbec3daa265b239f5
Ahmed Shalaby <ahmed.shalaby@bypa-ss.com>
2024-05-06 17:36:58 +0300

    wip: applying jsx as requirement

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
 import React from "react";
 
 const App: React.FC = () => {
-  return <></>;
+  const Categories = [
+    {
+      type: "Electroincs",
+      description: "Discover the latest tech gadgets and accessories",
+    },
+    {
+      type: "Apparel",
+      description: "Find the perfect outfit for any occasion",
+    },
+    {
+      type: "Home & Garden",
+      description: "Tansform your living space with our home decor",
+    },
+    {
+      type: "Beauty & Personal Care",
+      description: "Tansform your living space with our home decor",
+    },
+  ];
+
+  return (
+    <div>
+      <div>
+        <div>
+          <p>{"Shop by Category"}</p>
+          <p>{"Discover the perfect products for your needs."}</p>
+        </div>
+        <div>
+          <button>{"Electronics"}</button>
+          <button>{"Apparel"}</button>
+          <button>{"Home & Garden"}</button>
+          <button>{"Beauty & Personal Care"}</button>
+        </div>
+      </div>
+      <div>
+        {Categories &&
+          Categories?.map((category) => (
+            <div>
+              <img src="../public/vite.svg" />
+              <p>{category.type}</p>
+              <p>{category.description}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
